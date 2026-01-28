b043eaefefc5f5d279e24d485a526f6f30782a68
Snd-R <76580768+snd-r@users.noreply.github.com>
2026-01-28 15:16:58 +0300

    onnxruntime patch to use latest dawn

--- cmake/external/onnxruntime.cmake
+++ cmake/external/onnxruntime.cmake
@@ -11,6 +11,7 @@ else ()
         # WebGPU with static Dawn
         set(ONNXRUNTIME_BUILD_PARAMS
                 -Donnxruntime_USE_WEBGPU=ON
+                -Donnxruntime_BUILD_WEBGPU_EP_STATIC_LIB=ON
                 -Donnxruntime_WGSL_TEMPLATE=static
                 -Donnxruntime_BUILD_DAWN_SHARED_LIBRARY=OFF
         )
@@ -19,8 +20,10 @@ else ()
 
     ExternalProject_Add(ep_onnxruntime
             GIT_REPOSITORY https://github.com/microsoft/onnxruntime
-            GIT_TAG v1.23.0
+            GIT_TAG 727db0d3dc9f7dc5958891d80c1073ef7190f316
             GIT_PROGRESS 1
+            UPDATE_DISCONNECTED True
+            PATCH_COMMAND git apply ${CMAKE_CURRENT_LIST_DIR}/patches/onnxruntime_dawn_new_version.patch
             SOURCE_SUBDIR cmake
             CMAKE_ARGS
                 ${EP_CMAKE_ARGS}
